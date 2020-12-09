0976d75b10b9b40330fe41dcc0f7ae47d6617775
Daniel Coulson <danycoulson@hotmail.com>
2020-12-08 17:38:25 -0800

    init

---
+++ server/knexfile.js
@@ -0,0 +1,10 @@
+module.exports = {
+  client: "mysql",
+  connection: {
+    host: "127.0.0.1",
+    user: "<your_db_username>",
+    password: "<your_db_password>",
+    database: "<your_schema_name>",
+    charset: "utf8",
+  },
+};
---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());

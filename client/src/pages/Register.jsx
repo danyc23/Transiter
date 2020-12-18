178106956634830ed0743ab18c91aa5eca4d137a
Daniel Coulson <danycoulson@hotmail.com>
2020-12-17 23:16:16 -0800

    First MVP DONE

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -61,108 +61,115 @@ class Register extends React.Component {
     return (
       <section className="register">
         <Navbar />
-        <h1 className="register-title">Register</h1>
-        <form className="register-form" onSubmit={this.submitHandler}>
-          <div className="register-container">
-            <label className="register-label" for="companyName">
-              Company Name
-            </label>
-            <input
-              className="register-input"
-              type="text"
-              name="companyName"
-              id="companyName"
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div className="register-container">
-            <label className="register-label" for="country">
-              Country
-            </label>
-            <input
-              className="register-input"
-              type="text"
-              name="country"
-              id="country"
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div className="register-container">
-            <label className="register-label" for="regNumber">
-              Registration Number
-            </label>
-            <input
-              className="register-input"
-              type="text"
-              name="regNumber"
-              id="regNumber"
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div className="register-container">
-            <label className="register-label" for="address">
-              Address
-            </label>
-            <input
-              className="register-input"
-              type="text"
-              name="address"
-              id="address"
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div className="register-container">
-            <label className="register-label" for="phone">
-              Phone Number
-            </label>
-            <input
-              className="register-input"
-              type="number"
-              name="phone"
-              id="phone"
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div className="register-container">
-            <label className="register-label" for="email">
-              Email
-            </label>
-            <input
-              className="register-input"
-              type="text"
-              name="email"
-              id="email"
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div className="register-container">
-            <label className="register-label" for="password">
-              Password
-            </label>
-            <input
-              className="register-input"
-              type="password"
-              name="password"
-              id="password"
-              onChange={this.changeHandler}
-            />
-          </div>
-          <div className="register-container">
-            <button className="register-btn" type="submit">
-              Register
-            </button>
-          </div>
-          <div className="register-container register-container__cancel">
-            <div className="register-cancel">
-              <Link className="register-cancel__text" to="/">
-                CANCEL
-              </Link>
+        <div className="register-main__container">
+          <div className="register-border">
+            <div className="register-title__container">
+              <h1 className="register-title">Register</h1>
             </div>
-            <Link to="./register">
-              <p className="">Login</p>
-            </Link>
+
+            <form className="register-form" onSubmit={this.submitHandler}>
+              <div className="register-container">
+                <label className="register-label" for="companyName">
+                  Company Name
+                </label>
+                <input
+                  className="register-input"
+                  type="text"
+                  name="companyName"
+                  id="companyName"
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="register-container">
+                <label className="register-label" for="country">
+                  Country
+                </label>
+                <input
+                  className="register-input"
+                  type="text"
+                  name="country"
+                  id="country"
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="register-container">
+                <label className="register-label" for="regNumber">
+                  Registration Number
+                </label>
+                <input
+                  className="register-input"
+                  type="text"
+                  name="regNumber"
+                  id="regNumber"
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="register-container">
+                <label className="register-label" for="address">
+                  Address
+                </label>
+                <input
+                  className="register-input"
+                  type="text"
+                  name="address"
+                  id="address"
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="register-container">
+                <label className="register-label" for="phone">
+                  Phone Number
+                </label>
+                <input
+                  className="register-input"
+                  type="number"
+                  name="phone"
+                  id="phone"
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="register-container">
+                <label className="register-label" for="email">
+                  Email
+                </label>
+                <input
+                  className="register-input"
+                  type="text"
+                  name="email"
+                  id="email"
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="register-container">
+                <label className="register-label" for="password">
+                  Password
+                </label>
+                <input
+                  className="register-input"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.changeHandler}
+                />
+              </div>
+              <div className="register-container">
+                <button className="register-btn" type="submit">
+                  Register
+                </button>
+              </div>
+              <div className="register-container register-container__cancel">
+                <div className="register-cancel">
+                  <Link className="register-cancel__text" to="/">
+                    <button className="register-cancel__btn">CANCEL</button>
+                  </Link>
+                </div>
+                <Link to="./login">
+                  <p className="register-login__text">Login</p>
+                </Link>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </section>
     );
   }
